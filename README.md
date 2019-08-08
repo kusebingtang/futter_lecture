ff050e6131f77ef49c712fdc94a45a6647c15689
kusebingtang <kusebintang@163.com>
2019-08-08 21:14:38 +0800

    轮播图Banner功能开发

--- README.md
+++ README.md
@@ -14,4 +14,11 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our 
 [online documentation](https://flutter.dev/docs), which offers tutorials, 
 samples, guidance on mobile development, and a full API reference.
-# futter_lecture
+
+# flutter_lecture
+
+#### 轮播图Banner功能的开发
+* pubspec.yaml config flutter_swiper 插件
+```jshelllanguage
+flutter_swiper : ^1.1.6
+```
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,10 +12,32 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+
+    List _imageUrls=[
+      'http://pages.ctrip.com/commerce/promote/20180718/yxzy/img/640sygd.jpg',
+      'http://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg',
+      'http://dimg04.c-ctrip.com/images/700c10000000pdili7D8B_780_235_57.jpg',
+    ];
+
+
     return Scaffold(
       body: Center(
-        child: Text('首页'),
+        child:  Column(
+            children: <Widget>[
+              Container(
+                height: 160,
+                color: Colors.blueGrey,
+                child: Swiper(
+                  autoplay: true,
+                  itemCount: _imageUrls.length,
+                  itemBuilder: (BuildContext context,int index){
+                    return Image.network(_imageUrls[index],fit: BoxFit.fill);
+                  },
+                  pagination: SwiperPagination(),  //现实指示器
+                ),
+              )
+            ]
+        )
       ),
     );
   }
